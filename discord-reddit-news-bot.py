34c6ee422403ef0969804dd041b3e1e3595ec303
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-09-01 13:07:29 -0400

    fix: Removed similar code and fixed the log spamming bug when the page wasn't available
    Also, moved the subreddit url to the .env file for ease of use, and removed some log messages that were spammy

--- discord-reddit-news-bot.py
+++ discord-reddit-news-bot.py
@@ -10,69 +10,73 @@ load_dotenv()
 global latest_post
 latest_post = None
 
-# Remember to fill the .env file with the webhook to YOUR Discord channel
+# Remember to fill the .env file with your urls otherwise it won't work
 WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK")
-
-# Change the subreddit URL to whatever you want to see in your Discord Webhook (I like dead by daligght :) )
-SUBREDDIT_URL = "https://www.reddit.com/r/deadbydaylight/new/.rss"
+SUBREDDIT_URL = os.getenv("PAGE_URL")
 
 
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'"
     }
 
+
+
+# Calling the root logger
+logger = logging.getLogger()
+logging.basicConfig(filename = 'logs.log', encoding = 'utf-8', level = logging.INFO, format = "[%(levelname)s]  %(asctime)s | %(message)s")
+logger.info("Opened the script")
 print("Opened the script")
 
-while True:
-    try:
-        response = requests.get(SUBREDDIT_URL, headers=header)
-        feed = feedparser.parse(response.text)
-
-        # Add a check here to avoid the app from crashing
-        latest_feed = feed.entries[0]
-        # Calling the root logger
-        logger = logging.getLogger()
-        logging.basicConfig(filename = 'logs.log', encoding = 'utf-8', level = logging.INFO, format = "[%(levelname)s]  %(asctime)s | %(message)s")
 
 
-        # Check if it received all the entries
-        if len(feed.entries) > 0:
-            # The F stands for formatted, and the {} are a placeholder space for the text 
-            logger.info(f"Title: {latest_feed.title}, Link: {latest_feed.link}, ID: {latest_feed.id}") 
+try:
+    while True:
+        try:
+            response = requests.get(SUBREDDIT_URL, headers=header)
+            feed = feedparser.parse(response.text)
 
-        else:
-            # Cute print using the Godot structure, oh I feel nostalgia
-            #print("nos blokiaron los de: " + str(SUBREDDIT_URL_DBD) + ", bro")
-            logger.error("Nothing found, the page is down or we did too many requests")
 
-        post_title = latest_feed.title
-        post_link = latest_feed.link
-        post_id = latest_feed.id
+            # Check to prevent a crash if the feedparser doesn't grab the page
+            if feed.entries:
 
-        # The check to send the discord message if the ID is NOT the same
-        if post_id != latest_post:
+                latest_feed = feed.entries[0]
+                
+            else:
+                # Cute print using the Godot structure, oh I feel nostalgia, I won't delete this comment
+                #print("nos blokiaron los de: " + str(SUBREDDIT_URL_DBD) + ", bro")
+                logger.error(f"Page empty")
+
+            post_title = latest_feed.title
+            post_link = latest_feed.link
+            post_id = latest_feed.id
+
+            # The check to send the discord message if the ID is NOT the same
+            if post_id != latest_post:
             
-            # This is the message structure, you can change it to however you want
-            # \n is a new line, Discord also supports bold text and italics with ** and * respectively
-            discord_message = "**New Reddit post**\n" + "Link: " + post_link
+                # This is the message structure, you can change it to however you want
+                # \n is a new line, Discord also supports bold text and italics with ** and * respectively
+                discord_message = "**New Reddit post**\n" + "Link: " + post_link
 
 
 
 
-            requests.post(WEBHOOK_URL, json={"content": str(discord_message)})
-            latest_post = post_id
-            logger.info("Found a new post, yummy")
+                requests.post(WEBHOOK_URL, json={"content": str(discord_message)})
+                latest_post = post_id
+                logger.info(f"Retrieving a new post: Title: {post_title}, Link: {post_link}")
 
-        else:
+
+            else:
             
-            logger.info("Didn't find a new post")
+                logger.info("Didn't find a new post")
 
-    # Add a message upon closing the app
-    #except KeyboardInterrupt:
-        #logger.info("Closed the app manually")
+            
+        except Exception as error:
 
-    except Exception as error:
+            logger.error("This is an error, something exploded along the way")
 
-        logger.error("This is an error, something exploded along the way")
+        
+        time.sleep(30)
 
-    time.sleep(30)
+except KeyboardInterrupt:
+    print("Closed the script")
+    logger.info("Closed the script manually")
